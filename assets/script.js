095fbc88f270cff3c264d0ce8a7a2db8b74be1e2
bmaggiano <brandon.maggiano@gmail.com>
2022-09-22 16:08:08 -0700

    updated js for current time, background color will be red

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 //need a function to show current date at the top of the screen
 const today = moment().format("MMMM Do, YYYY")
 const time = moment().format("LTS")
+const hour = moment().format("k")
+
+console.log(typeof(hour)) //hour is a string
 
 $('#currentDay').text(today) 
 $('#currentTime').text(time)
@@ -8,7 +11,42 @@ $('#currentTime').text(time)
 setInterval(() => 
 $('#currentTime').text(new Date().toLocaleTimeString()),1000);
 
-
+if (hour == "8") {
+    $('#time-one').attr("style", "background-color:red;")
+    $('#form-one').attr("style", "background-color:red;")
+}
+if (hour == "9") {
+    $('#time-two').attr("style", "background-color:red;")
+    $('#form-two').attr("style", "background-color:red;")
+}
+if (hour == "10") {
+    $('#time-three').attr("style", "background-color:red;")
+    $('#form-three').attr("style", "background-color:red;")
+}
+if (hour == "11") {
+    $('#time-four').attr("style", "background-color:red;")
+    $('#form-four').attr("style", "background-color:red;")
+}
+if (hour == "12") {
+    $('#time-five').attr("style", "background-color:red;")
+    $('#form-five').attr("style", "background-color:red;")
+}
+if (hour == "13") {
+    $('#time-six').attr("style", "background-color:red;")
+    $('#form-six').attr("style", "background-color:red;")
+}
+if (hour == "14") {
+    $('#time-seven').attr("style", "background-color:red;")
+    $('#form-seven').attr("style", "background-color:red;")
+}
+if (hour == "15") {
+    $('#time-eight').attr("style", "background-color:red;")
+    $('#form-eight').attr("style", "background-color:red;")
+}
+if (hour == "16") {
+    $('#time-nine').attr("style", "background-color:red;")
+    $('#form-nine').attr("style", "background-color:red;")
+}
 
 
 //timeblocks color is green if current time is less than calendar time
