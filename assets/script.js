c42d297ac68a7690d4dcb58fa193d52f48c1b6ce
bmaggiano <brandon.maggiano@gmail.com>
2022-09-22 09:57:00 -0700

    comments for js file to psuedo code

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+//need a function to show current date at the top of the screen
+
+//need time blocks for 8am-5pm
+
+//timeblocks color is green if current time is less than calendar time
+//timeblocks color is red if current time is greater than calendar time
+//timeblocks color is oragne if current time is equal to current time block
+
+//timeblocks need to be textarea to input text
+
+//use append child to add button to each textarea
+//on click for button, be able to update/save the text the user enters into the text area
+//new text value should save in local storage localStorage.setItem()
+
+//when user refreshes the page the most recent saved changes persist
+
+
+
+
+
+//we can do this with js only or we can use HTML markup and run functions only in js
+
